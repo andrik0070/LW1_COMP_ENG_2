e1a2dbdab04c185e82d03a74e4d3dc3d88411e08
Andrew <andrik0070@gmail.com>
2018-10-31 18:43:18 +0200

    Fix

--- classes/lexic.py
+++ classes/lexic.py
@@ -2,11 +2,14 @@ import string
 
 
 class Token(object):
+    Identifier = 'Identifier'
+    Integer = 'Integer'
+    Delimiter = 'Delimiter'
+    DoubleDelimiter = 'DoubleDelimiter'
+    Keyword = 'Keyword'
+    Literal = 'Literal'
     eof = 'END-OF-FILE'
-    ident = 'IDENT'
-    number = 'NUMBER'
     operator = 'OP'
-    keyword = 'KEYWORD'
     block_start = 'START'
     block_end = 'END'
 
@@ -24,11 +27,11 @@ class Token(object):
 
 
 class Lexer(object):
-
     eof_marker = '$'
     whitespace = ' \t\n'
     newline = '\n'
-    comment_marker = '#'
+    # comment_marker = '#'
+    delimiters = ['.', ':', ';', '(', ')', '=', '-', '+', ',', '[', ']', '<', '>']
 
     def __init__(self, code):
         super(Lexer, self).__init__()
@@ -61,9 +64,9 @@ class Lexer(object):
                 char = self.get_next_char()
 
             # comment
-            elif char in Lexer.comment_marker:
-                while char not in Lexer.newline:
-                    char = self.get_next_char()
+            # elif char in Lexer.comment_marker:
+            #     while char not in Lexer.newline:
+            #         char = self.get_next_char()
 
             # identifier token
             elif char in string.ascii_letters:
@@ -72,14 +75,14 @@ class Lexer(object):
                 while char in (string.ascii_letters + string.digits):
                     match += char
                     char = self.get_next_char()
-                token = Token(Token.ident, match, self.lines[self.line_no], self.line_no, self.line_pos)
+                token = Token(Token.Identifier, match, self.lines[self.line_no], self.line_no, self.line_pos)
 
                 if match in Token.keywords:
-                    token.type = Token.keyword
+                    token.type = Token.Keyword
 
                 self.tokens.append(token)
 
-            # number token
+            # Integer token
             elif char in string.digits:
                 match = char
                 char = self.get_next_char()
@@ -87,29 +90,58 @@ class Lexer(object):
                     match += char
                     char = self.get_next_char()
 
-                token = Token(Token.number, match, self.lines[self.line_no], self.line_no, self.line_pos)
+                if char not in (self.delimiters + [' ']):
+                    raise ValueError()
+
+                token = Token(Token.Integer, match, self.lines[self.line_no], self.line_no, self.line_pos)
                 self.tokens.append(token)
+            elif char == "'":
+                match = char
+                char = self.get_next_char()
+                while char != "'":
+                    match += char
+                    char = self.get_next_char()
+                match += char
+                self.tokens.append(Token(Token.Literal, match + char, self.lines[self.line_no], self.line_no,
+                                         self.line_pos))
+                char = self.get_next_char()
+
+            elif char in self.delimiters:
+                match = char
+                char = self.get_next_char()
+
+                if (match + char) in [':=', '<>']:
+                    token = Token(Token.DoubleDelimiter, match + char, self.lines[self.line_no], self.line_no,
+                                  self.line_pos)
+                    char = self.get_next_char()
+                else:
+                    token = Token(Token.Delimiter, match, self.lines[self.line_no], self.line_no,
+                                  self.line_pos)
+                self.tokens.append(token)
+
 
             # operators
-            elif char in '+-*/=<>':
-                token = Token(Token.operator, char, self.lines[self.line_no], self.line_no, self.line_pos)
-                self.tokens.append(token)
-                char = self.get_next_char()
-
-            # start block
-            elif char == '{':
-                token = Token(Token.block_start, char, self.lines[self.line_no], self.line_no, self.line_pos)
-                self.tokens.append(token)
-                char = self.get_next_char()
-
-            # end block
-            elif char == '}':
-                token = Token(Token.block_end, char, self.lines[self.line_no], self.line_no, self.line_pos)
-                self.tokens.append(token)
-                char = self.get_next_char()
+            # elif char in '+-*/=<>':
+            #     token = Token(Token.operator, char, self.lines[self.line_no], self.line_no, self.line_pos)
+            #     self.tokens.append(token)
+            #     char = self.get_next_char()
+            #
+            # # start block
+            # elif char == '{':
+            #     token = Token(Token.block_start, char, self.lines[self.line_no], self.line_no, self.line_pos)
+            #     self.tokens.append(token)
+            #     char = self.get_next_char()
+            #
+            # # end block
+            # elif char == '}':
+            #     token = Token(Token.block_end, char, self.lines[self.line_no], self.line_no, self.line_pos)
+            #     self.tokens.append(token)
+            #     char = self.get_next_char()
 
             else:
-                raise ValueError('Unexpected character found: {0}:{1} -> {2}\n{3}'.format(self.line_no + 1, self.line_pos + 1, char, self.lines[self.line_no]))
+                raise ValueError(
+                    'Unexpected character found: {0}:{1} -> {2}\n{3}'.format(self.line_no + 1, self.line_pos + 1, char,
+                                                                             self.lines[self.line_no]))
 
         # end of file token
         token = Token(Token.eof, char, None, self.line_no, self.line_pos)
